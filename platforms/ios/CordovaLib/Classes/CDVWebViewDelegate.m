bc926cc0941f9627c9bba433f2ba42de3cea9797
mlc <mlc@17138862-0172-114e-a870-43eb0320d916>
2014-12-16 14:05:05 +0000

    New Beta release for Mobile ECT; Added support for open external links on safari
    git-svn-id: https://srvptsvn/svn/docs/trunk/company/Engineering/Projects/MobileApp/OutSystems/branches/ECT@18564 17138862-0172-114e-a870-43eb0320d916

--- platforms/ios/CordovaLib/Classes/CDVWebViewDelegate.m
+++ platforms/ios/CordovaLib/Classes/CDVWebViewDelegate.m
@@ -206,6 +206,23 @@ static NSString *stripFragment(NSString* url)
         shouldLoad = [_delegate webView:webView shouldStartLoadWithRequest:request navigationType:navigationType];
     }
 
+    if(shouldLoad){
+        NSLog(@"Request scheme: %@",[request.URL scheme]);
+        
+        NSString *external = @"external:";
+        
+        if ([request.URL.absoluteString hasPrefix:external])
+        {
+            NSLog(@"hacked link");
+            // JS-hacked URl is a target=_blank url - manually open the browser.
+            NSURL *url = [NSURL URLWithString:[request.URL.absoluteString substringFromIndex:external.length]];
+            [[UIApplication sharedApplication] openURL:url];
+            return YES;
+        }
+        
+    }
+    
+    
     VerboseLog(@"webView shouldLoad=%d (before) state=%d loadCount=%d URL=%@", shouldLoad, _state, _loadCount, request.URL);
 
     if (shouldLoad) {
