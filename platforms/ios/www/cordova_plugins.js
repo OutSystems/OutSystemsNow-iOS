e63e1dc92deed1eb205ea7e1acd57025fa39b4e6
Marco Costa <marco.costa@outsystems.com>
2015-11-24 11:51:01 +0000

    Cordova plugin Geolocation update

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -270,34 +270,6 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
-        "id": "org.apache.cordova.geolocation.Coordinates",
-        "clobbers": [
-            "Coordinates"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
-        "id": "org.apache.cordova.geolocation.PositionError",
-        "clobbers": [
-            "PositionError"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
-        "id": "org.apache.cordova.geolocation.Position",
-        "clobbers": [
-            "Position"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
-        "id": "org.apache.cordova.geolocation.geolocation",
-        "clobbers": [
-            "navigator.geolocation"
-        ]
-    },
-    {
         "file": "plugins/nl.x-services.plugins.calendar/www/Calendar.js",
         "id": "nl.x-services.plugins.calendar.Calendar",
         "clobbers": [
@@ -343,6 +315,34 @@ module.exports = [
         "clobbers": [
             "SQLitePlugin"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/Coordinates.js",
+        "id": "cordova-plugin-geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/Position.js",
+        "id": "cordova-plugin-geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -356,12 +356,12 @@ module.exports.metadata =
     "com.outsystems.loader": "1.0.0",
     "org.apache.cordova.file": "1.1.0",
     "org.apache.cordova.camera": "0.3.4-dev",
-    "org.apache.cordova.geolocation": "0.3.10",
     "nl.x-services.plugins.calendar": "4.3.1",
     "cordova-plugin-sim": "1.0.2",
     "cordova-plugin-network-information": "1.0.1-dev",
     "org.apache.cordova.device": "0.3.0",
-    "cordova-sqlite-storage": "0.7.13"
+    "cordova-sqlite-storage": "0.7.13",
+    "cordova-plugin-geolocation": "1.0.1"
 }
 // BOTTOM OF METADATA
 });
